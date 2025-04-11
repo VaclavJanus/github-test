7f30f53453c5ebc641e41d93737e51906e1e698b
VaclavJanus <vaclav.janus@vsb.cz>
2025-04-11 08:52:57 +0200

    Aktualizovat pxt.json, main.blocks, main.ts, main.py

--- main.ts
+++ main.ts
@@ -1,3 +1,13 @@
+smarthome.ledBrightness(AnalogPin.P0, false)
 basic.forever(function () {
-	
+    if (smarthome.dht11Sensor(DigitalPin.P0, smarthome.DHT11_state.DHT11_temperature_C) <= 25) {
+        smarthome.motorFan(AnalogPin.P1, true)
+        smarthome.ledBrightness(AnalogPin.P3, true)
+    } else {
+        smarthome.motorFan(AnalogPin.P1, false)
+        smarthome.ledBrightness(AnalogPin.P3, false)
+    }
+    if (true) {
+    	
+    }
 })
