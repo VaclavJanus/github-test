ccbf0a84034fda62b59b01fc45e97725bb3d6dd2
VaclavJanus <vaclav.janus@vsb.cz>
2025-04-11 09:09:29 +0200

    Aktualizovat main.ts, main.py

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
+smarthome.led_brightness(AnalogPin.P0, False)
+
 def on_forever():
-    pass
+    if smarthome.dht11_sensor(DigitalPin.P0, smarthome.DHT11_state.DHT11_TEMPERATURE_C) <= 25:
+        smarthome.motor_fan(AnalogPin.P1, True)
+        smarthome.led_brightness(AnalogPin.P3, True)
+    else:
+        smarthome.motor_fan(AnalogPin.P1, False)
+        smarthome.led_brightness(AnalogPin.P3, False)
+    if True:
+        pass
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,5 +1,5 @@
 smarthome.ledBrightness(AnalogPin.P0, false)
-basic.forever(function () {
+basic.forever(function on_forever() {
     if (smarthome.dht11Sensor(DigitalPin.P0, smarthome.DHT11_state.DHT11_temperature_C) <= 25) {
         smarthome.motorFan(AnalogPin.P1, true)
         smarthome.ledBrightness(AnalogPin.P3, true)
@@ -7,7 +7,9 @@ basic.forever(function () {
         smarthome.motorFan(AnalogPin.P1, false)
         smarthome.ledBrightness(AnalogPin.P3, false)
     }
+    
     if (true) {
-    	
+        
     }
+    
 })
